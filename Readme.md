374ee1b60cb02b331707394e4fdcc88ec6b72ce8
LucasEduardoRomero <lucasromero.cba@hotmail.com>
2021-04-17 17:00:02 -0400

    Starting readme documentation, Work features, prototype and ToDo's in index.js

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Rei Bot
+Rei é um bot feito para gerenciar o servidor Tecnocity do discord. Com comportameto previsível e gamificado, permite interações dinâmicas entre os participantes do servidor. Veja as Funcionalidades!
+
+## Funcionalidades
+Para a boa execução das funcionalidades, é necessário
+
+- [ ] Convidar pessoas para o servidor:  
+    Você pode convidar alguem chamando o comando  
+    `rei! getinvite`  
+    Ao chamar o comando, será debitado 5 moedas de seu usuário. Caso você não tenha, não será gerado o convite.  
+    __Cuidado ao gerar convites desnecessários, pois cada um custa moedas e duram 30 minutos_ ou até algum usuário usá-lo__
+    
+
+- [ ] Trabalho e geração de moeda:  
+    Em um canal nomeado __trampo__, é possível executar ação de trabalhar  
+    `rei! working`
+
+    O Resultado deve ser o retorno de Uma moeda do rei (moeda do servidor), que ficará salvo no banco de dados da API do bot. Você pode trabalhar uma vez por dia, ganhando uma moeda por trabalho.
+
+
--- index.js
+++ index.js
@@ -5,12 +5,17 @@ require('dotenv').config()
 client.on('ready', () => {
     console.log('back online again')
 })
-
+/* ToDo =>
+ Criar BD do Rei
+ Criar Models que comunicarão com BD do REI
+*/
 client.on('message', msg => {
+    
     const msgarray = msg.content.split(' ')
     if(msgarray[0]==='rei!') {
         switch (msgarray[1]) {
             case 'getinvite':
+                    /* ToDo => criar convite apenas se tiver mais de 5 moedas em conta. debitar as moedas do usuário */
                     const invite = msg.channel.createInvite({
                         maxUses: 1,
                         maxAge: 1800
@@ -19,11 +24,19 @@ client.on('message', msg => {
                         msg.channel.send(`<@${msg.author.id}> sua requisição foi enviada em private message.`)
                         msg.author.send(`https://discord.gg/${res.code}`)
                     })
+                    /* ToDo => quando novo usuário entrar, criar conta no BD do REI */
+                    break;
+
+            case 'working':
+                msg.channel.send(`Trampo Executado. Uma moeda do rei será salva em sua conta`)
+                /* Todo => guardar moeda no cadastro do REI */
                 break;
             default:
                 msg.channel.send('nao implementado')
                 break;
+            
         }
+        
     }
 })
 
